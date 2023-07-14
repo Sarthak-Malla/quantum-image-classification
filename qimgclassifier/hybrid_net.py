5eff5eebf2fbe87fa2a1cb7f0bdbe6f1b40bb0e2
Sarthak-Malla <spm9513@nyu.edu>
2023-07-14 11:39:04 +0400

    Used LeNet architecture

--- qimgclassifier/hybrid_net.py
+++ qimgclassifier/hybrid_net.py
@@ -174,30 +174,34 @@ class QuantumNet(nn.Module):
     """
     def __init__(self):
         super(QuantumNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.fc1 = nn.Linear(9216, config.input_size)
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.dropout = nn.Dropout2d()
+        self.fc1 = nn.Linear(256, 64)
+        self.fc2 = nn.Linear(64, config.input_size)
         self.bn1d = nn.BatchNorm1d(config.input_size)
         self.test_network = nn.ModuleList()
 
         self.test_network.append(QNet(config.input_size, config.shots, save_statevectors=True))
-        self.fc2 = nn.Linear(config.input_size, config.num_classes)
+        self.fc3 = nn.Linear(config.input_size, config.num_classes)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = torch.flatten(x, 1)
 
-        x = self.fc1(x)
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = self.dropout(x)
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
         if config.batch_norm:
             x = self.bn1d(x)
         x = np.pi * torch.sigmoid(x)
         for f in self.test_network:
             x = f(x)
 
-        x = self.fc2(x)
+        x = self.fc3(x)
         output = F.log_softmax(x, dim=1)
         return output
