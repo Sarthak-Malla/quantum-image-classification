90c8e35cb7a0de9923700bb01c77b2fbd3f795b3
Sarthak-Malla <spm9513@nyu.edu>
2023-07-14 11:18:52 +0400

    Update package module to support more functions

--- qimgclassifier/__init__.py
+++ qimgclassifier/__init__.py
@@ -1,6 +1,8 @@
 from .config import Config
 from .data_load import get_train_loader, get_test_loader
-from .hybrid_net import HybridNet, TorchNet, HybridCIFARNet
+from .hybrid_net import HybridNet, TorchNet, HybridCIFARNet, QNet, QuantumNet
 from .hybrid import Hybrid
 from .qnn import create_qnn
-from .qnet import HybridFunction, QuantumCircuit
+from .qnet import HybridFunction, QuantumCircuit, QuantumNetworkCircuit, QNetFunction
+from .deprecated import get_subsystems_counts, pauli_single
+from .gradients import *
